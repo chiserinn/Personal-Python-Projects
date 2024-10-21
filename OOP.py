f4d2da4d706fba1c904e56a0ccb37abd217fe481
Erin <erin.klarisa@gmail.com>
2024-10-21 19:46:42 +0800

    oop notes

---
+++ OOP.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################################################################################
+
+#Object Oriented Programming Notes
+
+## Object
+
+#class Dog: (CLASS)
+#   name = "Erin" (IDENTITY - unique key)
+#   type = "poodle" (STATE - attributes, variables)
+#   def bite(): (BEHAVIOUR - method)
+    # pass
+
+# obj = Dog() (OBJECT)
+
+# def __init__ (self,name):
+#    self.name = name
+# creates an instance method(called everytime Dog() is called) -> to initialise the object(self) and argument(name) to take the attribute (name)
+
+## Inheritance
+
+# class Dog():
+# class Poodle(Dog):
+# class Baby_Poodle (Poodle):
+# each subsequent class takes the methods and attributes
