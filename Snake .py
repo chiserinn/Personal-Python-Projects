d0555bc83986b09ac4f5a7e82df681d698193d20
Erin <erin.klarisa@gmail.com>
2024-10-23 14:46:08 +0800

    snake game

--- Snake .py
+++ Snake .py
@@ -25,14 +25,75 @@ game_window = pygame.display.set_mode((window_x, window_y))
 #fps
 fps = pygame.time.Clock()
 
+#snake
+snake_position = [100,50]
+
+snake_body = [ [100,50], [90,50], [80,50], [70,50]]
+
+#fruit
+fruit_position = [random.randrange(1, (window_x//10)) * 10, random.randrange(1,(window_y)//10)*10]
+
+fruit_spawn = True
+
+#default orientation
+direction = 'RIGHT'
+change_to = direction
+
+#score
+score = 0
+
+#display
+def show_score(choice,color,font,size):
+    #set font
+    score_font = pygame.font.SysFont(font,size)
+    #creating text surface
+    score_surface = score_font.render('Score: ' + str(score), True, color)
+    #continuously update text surface
+    score_rect = score_surface.get_rect()
+    #display
+    game_window.blit(score_surface, score_rect)
+
+#game over
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 50)
+    game_over_surface = my_font.render('Your score is: ' + str(score), True, red)
+    game_over_rect = game_over_surface.get_rect()
+
+    #set position 
+    game_over_rect.midtop = (window_x/2, window_y/4)
+
+    game_window.blit(game_over_surface, game_over_rect)
+    #screen.blit(set surface, update method)
+
+    time.sleep(2)
+    pygame.quit()
+    quit()
 
 
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'UPDOWN'   
+            if event.key == pygame.LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+    
+    if change_to == 'UP' and direction !='DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction !='UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction !='RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction !='LEFT':
+        direction = 'RIGHT'    
 
 
-
-
-
-
+    if direction == 'UP'    
 ###########################################################################################################################
 
 #Object Oriented Programming Notes
