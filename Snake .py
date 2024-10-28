28c06d4279e3f0d5f68fff621c28bb0c89ae1382
Erin <erin.klarisa@gmail.com>
2024-10-28 11:38:08 +0800

    snake add segment

--- Snake .py
+++ Snake .py
@@ -67,8 +67,8 @@ def game_over():
     quit()
 
 class Food:
-    def __init__(self):
-        self.position = self.generate_random_pos()
+    def __init__(self, snake_body):
+        self.position = self.generate_random_pos(snake_body)
         
     
     def draw(self):
@@ -78,31 +78,61 @@ class Food:
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y*cell_size,cell_size,cell_size)
         game_window.blit(food_surface, food_rect)
         #pygame.draw.rect(game_window, white, food_rect)
-
-    def generate_random_pos(self):
+    
+    def generate_random_cell(self):
         x = random.randint(0, number_of_cells - 1)
         y = random.randint(0, number_of_cells - 1)
-        position = Vector2(x,y)
+        return Vector2(x,y)
+
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+
+        while position in snake_body:
+            position = self.generate_random_cell()
+            
         return position
         
 class Snake:
     def __init__(self):
         self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
         self.direction = Vector2(1,0)
+        self.add_segment = False
     
     def draw(self):
         for segment in self.body:
             segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(game_window, blue, segment_rect, 0, 7)
 
+
     def update(self):
-        self.body = self.body[:-1]
         self.body.insert(0,self.body[0] + self.direction)
 
-food = Food()
-food_surface = pygame.image.load("ree-vector-red-apple-png_1020x.jpg")
-snake = Snake()
+        if self.add_segment == True:
+            self.add_segment = False
+        else:
+            self.body = self.body[:-1]
+        
 
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food(self.snake.body)
+
+    def draw(self):
+        self.food.draw()
+        self.snake.draw()
+
+    def update(self):
+        self.snake.update()
+        self.check_collision_food()
+
+    def check_collision_food(self):
+        if self.snake.body[0] == self.food.position:
+            self.food.position = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
+
+food_surface = pygame.image.load("ree-vector-red-apple-png_1020x.jpg")
+game = Game()
 
 SNAKE_UPDATE = pygame.USEREVENT
 
@@ -113,17 +143,17 @@ while True:
 
     for event in pygame.event.get():
         if event.type  == SNAKE_UPDATE:
-            snake.update()
+            game.update()
     
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP and snake.direction != (0,1):
-            snake.direction = Vector2(0, -1)
-        if event.key == pygame.K_DOWN and snake.direction != (0,-1):
-            snake.direction = Vector2(0, 1)
-        if event.key == pygame.K_LEFT and snake.direction != (1,0):
-            snake.direction = Vector2(-1, 0)
-        if event.key == pygame.K_RIGHT and snake.direction != (-1,0):
-            snake.direction = Vector2(1, 0)
+        if event.key == pygame.K_UP and game.snake.direction != (0,1):
+            game.snake.direction = Vector2(0, -1)
+        if event.key == pygame.K_DOWN and game.snake.direction != (0,-1):
+            game.snake.direction = Vector2(0, 1)
+        if event.key == pygame.K_LEFT and game.snake.direction != (1,0):
+            game.snake.direction = Vector2(-1, 0)
+        if event.key == pygame.K_RIGHT and game.snake.direction != (-1,0):
+            game.snake.direction = Vector2(1, 0)
 
     if event.type == pygame.QUIT:
         pygame.quit()
@@ -132,8 +162,7 @@ while True:
 
 
     game_window.fill(green)
-    food.draw()
-    snake.draw()
+    game.draw()
 
     pygame.display.update()
     fps.tick(60)
