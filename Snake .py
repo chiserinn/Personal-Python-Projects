d507fd625d559f33554bc9fcaa020ded1f4e09f7
Erin <erin.klarisa@gmail.com>
2024-10-30 01:22:31 +0800

    finishing snake code as oop example

--- Snake .py
+++ Snake .py
@@ -15,13 +15,14 @@ green = pygame.Color(100,255,0)
 
 cell_size = 30
 number_of_cells = 25
+offset = 70
 
 #initialisation
 pygame.init()
 
 #initialise game window
 pygame.display.set_caption('Snake!')
-game_window = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
+game_window = pygame.display.set_mode((2*offset + cell_size*number_of_cells, 2*offset + cell_size*number_of_cells))
 
 #fps
 fps = pygame.time.Clock()
@@ -34,10 +35,10 @@ snake_body = [ [100,50], [90,50], [80,50], [70,50]]
 
 fruit_spawn = True
 
-#default orientation
 
 #score
 score = 0
+
 
 #display
 def show_score(choice,color,font,size):
@@ -103,7 +104,6 @@ class Snake:
             segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(game_window, blue, segment_rect, 0, 7)
 
-
     def update(self):
         self.body.insert(0,self.body[0] + self.direction)
 
@@ -111,25 +111,49 @@ class Snake:
             self.add_segment = False
         else:
             self.body = self.body[:-1]
-        
+
+    def reset(self):
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
+        self.direction = Vector2(1,0)       
 
 class Game:
     def __init__(self):
         self.snake = Snake()
         self.food = Food(self.snake.body)
+        self.state = "RUNNING"
 
     def draw(self):
         self.food.draw()
         self.snake.draw()
 
     def update(self):
-        self.snake.update()
-        self.check_collision_food()
+
+        if self.state == "RUNNING":
+            self.snake.update()
+            self.check_collision_food()
+            self.check_collision_with_edges()
+            self.check_collision_with_tail()
 
     def check_collision_food(self):
         if self.snake.body[0] == self.food.position:
             self.food.position = self.food.generate_random_pos(self.snake.body)
             self.snake.add_segment = True
+        
+    def check_collision_with_edges(self):
+        if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
+            self.game_over()
+        if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1:
+            self.game_over()
+    
+    def game_over(self):
+        self.snake.reset()
+        self.food.position = self.food.generate_random_pos(self.snake.body)
+        self.state = "STOPPED"
+
+    def check_collision_with_tail(self):
+        headless_body = self.snake.body[1:]
+        if self.snake.body[0] in headless_body:
+            self.game_over()
 
 food_surface = pygame.image.load("ree-vector-red-apple-png_1020x.jpg")
 game = Game()
@@ -146,6 +170,9 @@ while True:
             game.update()
     
     if event.type == pygame.KEYDOWN:
+        if game.state == "STOPPED":
+            game.state = "RUNNING"
+
         if event.key == pygame.K_UP and game.snake.direction != (0,1):
             game.snake.direction = Vector2(0, -1)
         if event.key == pygame.K_DOWN and game.snake.direction != (0,-1):
@@ -154,7 +181,7 @@ while True:
             game.snake.direction = Vector2(-1, 0)
         if event.key == pygame.K_RIGHT and game.snake.direction != (-1,0):
             game.snake.direction = Vector2(1, 0)
-
+        
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
@@ -162,8 +189,8 @@ while True:
 
 
     game_window.fill(green)
+    pygame.draw.rect(game_window, black, (offset-5, offset-5, cell_size * number_of_cells +10,cell_size * number_of_cells +10), 5)
     game.draw()
-
     pygame.display.update()
     fps.tick(60)
         
