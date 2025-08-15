90a76e75dfc56befb53cdfca434eb3162b5113ab
Erin <erin.klarisa@gmail.com>
2025-08-16 01:16:24 +0800

    access modifiers

---
+++ access modifiers.py
@@ -0,0 +1,20 @@
+class ClassSchedule:
+   def __init__(self, course, instructor):
+       self._course = course # private
+       self._instructor = instructor # private
+ 
+   def display_course(self):
+       # public
+ 
+       print(f'Course: {self._course}, Instructor: {self._instructor}')
+ 
+
+sched = ClassSchedule('Biology', 'Ms. Smith')
+
+# the following will throw an Attribute Error because we're trying to access a private member
+#sched.__course 
+ 
+
+# this line won't throw an Attribute Error because this method is publi
+sched.display_course() 
+sched._course()
